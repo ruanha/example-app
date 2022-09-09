42cf92f54e25a9400b8694f06457316429f24c58
Rune Hartvig <rune_hartvig@hotmail.com>
2022-09-09 14:15:50 +0200

    finish details

--- src/app/features/details/details.component.ts
+++ src/app/features/details/details.component.ts
@@ -1,8 +1,26 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+
+import { FlickService } from "~/app/core/services/flick.service";
+import { FlickModel } from "~/app/core/models/flick.model";
 
 @Component({
   moduleId: module.id,
   selector: "ns-details",
   templateUrl: "details.component.html",
 })
-export class DetailsComponent {}
+export class DetailsComponent implements OnInit {
+  flick: FlickModel | undefined = undefined;
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private flickService: FlickService
+  ) {}
+
+  ngOnInit(): void {
+    const id = +this.activatedRoute.snapshot.params.id;
+    if (id) {
+      this.flick = this.flickService.getFlickById(id);
+    }
+  }
+}
