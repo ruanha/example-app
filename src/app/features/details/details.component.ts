62fe69c435304eb4154441ac23a02b3cf034d82b
Rune Hartvig <rune_hartvig@hotmail.com>
2022-09-09 13:33:25 +0200

    init details module

---
+++ src/app/features/details/details.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  moduleId: module.id,
+  selector: "ns-details",
+  templateUrl: "details.component.html",
+})
+export class DetailsComponent {}
